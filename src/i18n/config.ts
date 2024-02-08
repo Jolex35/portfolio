a4a28469d818fc9081a30d4a71852255a568e538
Jolex <jolex.ytb@gmail.com>
2024-02-08 15:00:45 +0100

    odkazomid

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -2,15 +2,11 @@ import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import en from "./en/translation.json";
-import pl from "./pl/translation.json";
 import fr from "./fr/translation.json";
 
 export const resources = {
   en: {
     translation: en,
-  },
-  pl: {
-    translation: pl,
   },
   fr: {
       translation: fr,
